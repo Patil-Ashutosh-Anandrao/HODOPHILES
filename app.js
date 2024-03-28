85329abb0f945de273a2cd3d451051c95970fa5f
Ashu Patil <ashutoshpa4367@gmail.com>
2024-03-28 18:24:43 +0530

    feat : updation

--- app.js
+++ app.js
@@ -26,38 +26,20 @@ const methodOverride = require('method-override');
 // require ejs-mate 
 const ejsMate = require('ejs-mate');
 
-// define ejs engine 
-app.engine('ejs', ejsMate);
+//require the wrapAsync module
+const wrapAsync = require('./public/util/wrapAsync.js');
 
-// require the express-session module
-app.use(methodOverride('_method'));
-
-
-
-
-
-
-
-// set the view engine to ejs
-app.set('view engine', 'ejs'); 
-// set the views directory
-app.set('views', path.join(__dirname, 'views')); 
-
-// to parse the data comes in the body of the request
-app.use(express.urlencoded({ extended: true }));
-
-
-
-// to use static files from public folder and inside it from css folder 
-app.use(express.static(path.join(__dirname, "/public")));
-
-
+// require the ExpressError module
+const ExpressError = require('./public/util/ExpressError.js');
 
 
 
 // Connect to the database
 const MONGO_URL= 'mongodb://127.0.0.1:27017/HODOPHILES'; 
 // HODOPHILES is the name of the database and this link is copied from the mongodb website 
+
+
+
 
 main()
     .then(() => console.log('Connected to MongoDB...'))
@@ -67,11 +49,45 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs'); 
+
+// set the views directory
+app.set('views', path.join(__dirname, 'views')); 
+
+// to parse the data comes in the body of the request
+app.use(express.urlencoded({ extended: true }));
+
+// require the express-session module
+app.use(methodOverride('_method'));
+
+// define ejs engine 
+app.engine('ejs', ejsMate);
+
+// to use static files from public folder and inside it from css folder 
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+
+
+
+// Basic API
+app.get('/', (req, res) => {
+    res.send('Hello I am Root  ');
+});
+
+
+
 // Create Index Route (fetch datafrom Db and show on webpage)
-app.get('/listings', async (req, res) => {
+app.get('/listings',
+        wrapAsync (async (req, res) => {
     const allListings = await Listing.find({}); 
     res.render("listings/index.ejs", { allListings});
-});
+})
+);
 
 
 
@@ -84,63 +100,78 @@ app.get('/listings/new', (req, res) => {
 
 
 
+
+// show Route 
+app.get('/listings/:id', 
+        wrapAsync (async (req, res) => {
+    let { id } = req.params;  // extract id
+    const listing = await Listing.findById(id); // find id and store data in listing
+    console.log(id);
+    res.render("listings/show.ejs", { listing }); // pass data for listing to show.ejs
+})
+);
+
+
+
+
 // Create Route
-app.post('/listings', async (req, res, next) => {
+app.post('/listings', 
+    wrapAsync (async (req, res, next) => {
 
     // extract data from the body of the request
     // const { title, description, price, location, country } = req.body; 
     // insted of this we will use listing array method in new.ejs  like listing[title], listing[description] and so on
     // and use below method 
-    try{
-        let newListing = new Listing (req.body.listing); // extract data from the body of the request    
+    
+        if (!req.body.listing) {
+            throw new ExpressError(400, 'Invalid Listing Data');
+        }
+        const newListing = new Listing (req.body.listing); // extract data from the body of the request    
         await newListing.save(); // save the listing to the database
         res.redirect("/listings"); // redirect to the index route
-    } 
-    catch (err) {
-        next(err);
-    } 
-
-});
+   
+})
+);
 
 
 
 // Edit Route
-app.get('/listings/:id/edit', async (req, res) => {
+app.get('/listings/:id/edit', 
+        wrapAsync (async (req, res) => {
     let { id } = req.params; // extract id
     const listing = await Listing.findById(id); // find id and store data in listing
     res.render("listings/edit.ejs", { listing }); // pass data for listing to edit.ejs
-});
+})
+);
 
 
 
 
 
 // update Route
-app.put('/listings/:id', async (req, res) => {
+app.put('/listings/:id', 
+        wrapAsync (async (req, res) => {
+            if (!req.body.listing) {
+                throw new ExpressError(400, 'Invalid Listing Data');
+            }
     let { id } = req.params; // extract id
     await Listing.findByIdAndUpdate(id, { ...req.body.listing  }); // find id and update data in listing
     res.redirect(`/listings/${id}`); // redirect to the show route
-});
+})
+);
 
 
 // Delete Route 
-app.delete('/listings/:id', async (req, res) => {
+app.delete('/listings/:id', 
+            wrapAsync (async (req, res) => {
     let { id } = req.params; // extract id
     let deletedListing = await Listing.findByIdAndDelete(id); 
     // find id and delete data in listing
 
     res.redirect("/listings"); // redirect to the index route
-});
+})
+);
 
-
-
-// show Route 
-app.get('/listings/:id', async (req, res) => {
-    let { id } = req.params;  // extract id
-    const listing = await Listing.findById(id); // find id and store data in listing
-    console.log(id);
-    res.render("listings/show.ejs", { listing }); // pass data for listing to show.ejs
-});
 
 
 
@@ -162,19 +193,28 @@ app.get('/listings/:id', async (req, res) => {
 
 
 
+// // Create new route for testing listing
+// // for above urls not match means page not found 
+// // so we will write code for that
+ app.all('*', (req, res, next) => {
+    next(new ExpressError(404,'Page Not Found'));
+ });
+
+
+
+
+
 // middleware for custome error handling 
 app.use((err, req, res, next) => {
-    res.send("Something went Wrong !");
+
+    // deconstruct the error object
+    let {statusCode = 500, message="Something went Wrong !"} = err;
+
+    // send error 
+    res.status(statusCode).send(message);
+
 });
 
-
-
-
-
-// Basic API
-app.get('/', (req, res) => {
-    res.send('Hello I am Root  ');
-});
 
 
 
