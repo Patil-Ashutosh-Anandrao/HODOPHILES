8e15828e6cea08537c865179106a66d2e15d8691
Ashu Patil <ashutoshpa4367@gmail.com>
2024-03-30 11:58:31 +0530

    feat : changes

---
+++ models/review.js
@@ -0,0 +1,22 @@
+// require mongoose
+const mongoose = require('mongoose');
+
+// store mongoose schema in schema varialble and use schema variable every where insten of using mongoose.schema 
+const Schema = mongoose.Schema;
+
+// create review schema
+const reviewSchema = new Schema({
+    comment: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now() // this will set the current date and time
+    }
+});
+
+// export module to use in another files like app.js
+module.exports = mongoose.model('Review', reviewSchema);
