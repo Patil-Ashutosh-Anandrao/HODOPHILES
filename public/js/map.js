803d6d99f7f472e0f3b6aa70a48df172da1ce6e2
Ashu Patil <ashutoshpa4367@gmail.com>
2024-04-13 14:48:39 +0530

    feat : change

--- public/js/map.js
+++ public/js/map.js
@@ -10,14 +10,18 @@
                 const map = new mapboxgl.Map({
                     container: 'map', // container ID
                     style:"mapbox://styles/mapbox/streets-v12", // style URL
-                    center: [73.8567, 18.5204], // starting position [lng, lat]
+                    center: [77.209, 28.6139], // starting position [lng, lat]
                     zoom: 9 // starting zoom
                 });
 
                 // create new marker 
                 console.log(coordinates);
-                
-                const marker = new mapboxgl.Marker()
-                .setLngLat(coordinates) // listing.geometry.coordinates 
-                .addTo(map);
 
+                const marker = new mapboxgl.Marker({color:"red"})
+                .setLngLat(coordinates)
+                .setPopup(
+                    new mapboxgl.Popup({offset:25}).setHTML(
+                    `<h5>${listing.title}</h5><p>Exact location will be provided after booking</p>`
+                    )
+                )
+                .addTo(map);
